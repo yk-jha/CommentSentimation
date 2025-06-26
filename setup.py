3b36ef71f661ab76d39ddc7bd53e2396cb4b7805
yk-jha <yjha5965@gmail.com>
2025-06-26 14:57:16 +0530

    Setup and Requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'YashJha',
+    author_email= 'yjha5965@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
